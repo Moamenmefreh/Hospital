253e4a8c82117cae2fac8cf70d967ac32dd85f45
Moamenmefreh <moamenmefreh@gmail.com>
2026-04-26 16:08:37 +0300

    add controller in solution

--- Hospital/Controllers/WeatherForecastController.cs
+++ Hospital/Controllers/WeatherForecastController.cs
@@ -10,6 +10,10 @@ namespace Hospital.Controllers
 		{
 			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 		};
+		private static readonly string[] Summaries2 = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
 
 		private readonly ILogger<WeatherForecastController> _logger;
 
